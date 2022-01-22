b2a148d44e2cb8178fb925d3b432f8bcb0eade6a
poruruba <hiuraba@gmail.com>
2022-01-22 12:53:28 +0900

    modify

--- scripts/copyall.js
+++ scripts/copyall.js
@@ -2,4 +2,5 @@ const fse = require('fs-extra');
 
 const THIS_BASE_PATH = __dirname + "/..";
 
+console.log("copy /public to /dist");
 fse.copySync(THIS_BASE_PATH + "/public", THIS_BASE_PATH + "/dist");
