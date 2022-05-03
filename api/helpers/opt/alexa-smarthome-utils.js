9685e5b8c8608a009efeae98e6c29f3ba79807cc
poruruba <44470781+poruruba@users.noreply.github.com>
2022-05-03 10:40:30 +0900

    Update alexa-smarthome-utils.js

--- api/helpers/opt/alexa-smarthome-utils.js
+++ api/helpers/opt/alexa-smarthome-utils.js
@@ -7,15 +7,23 @@ class AlexaSmartHomeUtils{
         this.intentHandles.set(matcher, handle);
     }
 
+    defaultIntent( handle ){
+        this.defaultHandle = handle;
+    }
+
     handle(){
         return async (handlerInput, context) => {
             var intent = handlerInput.directive.header.namespace + '.' + handlerInput.directive.header.name;
-            console.log('intent: ' + intent);
+//            console.log('intent: ' + intent);
             var handler = this.intentHandles.get(intent);
-            if( handler )
+            if( handler ){
                 return handler(handlerInput, context);
-            else
-                console.log('not found intent: ' + intent);
+            }else{
+                if( this.defaultHandle )
+                    return this.defaultHandle(intent, handlerInput, context);
+                else
+                    console.log('not found intent: ' + intent);
+            }
         }
     }
 }
