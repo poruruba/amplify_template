c97da4dd2aa32678ad53a791565151defc8bc792
poruruba <44470781+poruruba@users.noreply.github.com>
2022-06-25 16:37:45 +0900

    Create lambda.js

---
+++ api/helpers/opt/lambda.js
@@ -0,0 +1,53 @@
+const fetch = require('node-fetch');
+const Headers = fetch.Headers;
+
+class LambdaProxy{
+  constructor(base_url){
+    this.base_url = base_url;
+  }
+
+  invoke(params, callback){
+    if(params.InvocationType == 'RequestResponse'){
+      do_post( this.base_url + params.FunctionName, params.body )
+      .then(response =>{
+        callback(null, {
+          StatusCode: 200,
+          Payload: JSON.stringify(response)
+        });
+      })
+      .catch(error =>{
+        callback(error);
+      });
+    }else
+    if( params.InvocationType == 'Event' ){
+      do_post( this.base_url + params.FunctionName, params.body )
+      .then(response =>{
+        console.log("LambdaProxy", response);
+      })
+      .catch(error =>{
+        console.error("LambdaProxy", error);
+      });
+
+      callback(null, {
+        StatusCode: 200,
+        Payload: ""
+      });
+    }
+  }
+}
+
+function do_post(url, body) {
+  const headers = new Headers({ "Content-Type": "application/json" });
+  return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: headers
+    })
+    .then((response) => {
+      if (!response.ok)
+        throw 'status is not 200';
+      return response.json();
+    });
+}
+
+module.exports = LambdaProxy;
