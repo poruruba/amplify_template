03a6650f2334ee4316578bd46dfbdb4a9eed0bd2
poruruba <44470781+poruruba@users.noreply.github.com>
2022-07-31 20:57:27 +0900

    Update lambda_deploy.js

--- scripts/lambda_deploy.js
+++ scripts/lambda_deploy.js
@@ -3,34 +3,41 @@
 const THIS_BASE_PATH = __dirname + "/..";
 
 const CONTROLLERS_BASE = THIS_BASE_PATH + '/api/controllers/';
-const ZIP_FNAME = 'index.zip';
+const ZIP_FNAME = 'deploy.zip';
 
 const AWS = require('aws-sdk');
 AWS.config.update({
-  region: "ap-northeast-1",
+	region: "ap-northeast-1",
 });
-const lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
+const lambda = new AWS.Lambda({ apiVersion: '2015-03-31' });
+const TEMP_FOLDER = THIS_BASE_PATH + '/temp/';
 
 const fs = require('fs');
 const archiver = require('archiver');
 
-async function folderToZip(target_folder){
-	return new Promise((resolve, reject) =>{
-		const output = fs.createWriteStream(CONTROLLERS_BASE + target_folder + '/' + ZIP_FNAME);
+async function folderToZip(target_folder) {
+	try{
+		fs.mkdirSync(TEMP_FOLDER);
+	}catch(error){
+		console.log('temp folder already exists');
+	}
+
+	return new Promise((resolve, reject) => {
+		const output = fs.createWriteStream(TEMP_FOLDER + ZIP_FNAME);
 		const archive = archiver('zip', {
-		  zlib: { level: 9 }
+			zlib: { level: 9 }
 		});
 
-		output.on('close', function() {
-		  console.log(CONTROLLERS_BASE + target_folder + '/' + ZIP_FNAME + ': ' + archive.pointer() + ' total bytes');
-		  resolve();
+		output.on('close', function () {
+			console.log(CONTROLLERS_BASE + TEMP_FOLDER + ZIP_FNAME + ': ' + archive.pointer() + ' total bytes');
+			resolve();
 		});
 
-		output.on('end', function() {
-		  console.log('Data has been drained');
+		output.on('end', function () {
+			console.log('Data has been drained');
 		});
 
-		archive.on('error', function(err) {
+		archive.on('error', function (err) {
 			reject(err);
 		});
 
@@ -40,34 +47,34 @@ async function folderToZip(target_folder){
 	});
 }
 
-if( process.argv.length < 3 ){
+if (process.argv.length < 3) {
 	console.log('usage: npm run lambda_deploy [folder_name]');
 	return;
 }
 
 var folder_name = process.argv[2];
-var target_folder = CONTROLLERS_BASE + folder_name + '/';
 
-(async () =>{
-	try{
+(async () => {
+	try {
 		await folderToZip(folder_name);
-		
-		var binary = fs.readFileSync(target_folder + ZIP_FNAME);
+
+		var binary = fs.readFileSync(TEMP_FOLDER + ZIP_FNAME);
 
 		var params = {
-		  FunctionName: folder_name,
-		  ZipFile: binary
+			FunctionName: folder_name,
+			ZipFile: binary
 		};
 		lambda.updateFunctionCode(params, (err, data) => {
-		  if (err){
-		  	console.log(err, err.stack);
-		  	return;
-		  }
-		  console.log("Lambda Updated");
-		  console.log("FunctionArn: " + data.FunctionArn);
-		  console.log("Runtime: " + data.Runtime);
+			if (err) {
+				console.log(err, err.stack);
+				return;
+			}
+			console.log("Lambda Deployed");
+			console.log("FunctionName: " + data.FunctionName);
+			console.log("FunctionArn: " + data.FunctionArn);
+			console.log("Runtime: " + data.Runtime);
 		});
-	}catch(error){
+	} catch (error) {
 		console.error(error);
 	}
 })();
