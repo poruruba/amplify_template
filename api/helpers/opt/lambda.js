0e338886aefaadce39db6c2a7a7d44ca522a9a80
poruruba <44470781+poruruba@users.noreply.github.com>
2022-07-31 17:02:24 +0900

    Update lambda.js

--- api/helpers/opt/lambda.js
+++ api/helpers/opt/lambda.js
@@ -3,131 +3,120 @@
 const THIS_BASE_PATH = process.env.THIS_BASE_PATH;
 const CONTROLLERS_BASE = THIS_BASE_PATH + '/api/controllers/';
 
-function lambdaInvoke(params, callback){
-  if(params.InvocationType == 'RequestResponse'){
-    var returned = false;
-    var return_response = (ret) =>{
-      if( !returned ){
-        returned = true;
+function lambdaInvoke(params, callback) {
+  var folder = CONTROLLERS_BASE + params.FunctionName;
+  var func = require(folder).handler;
+  if( !func ){
+    if( callback )
+      callback("lambdaInvoke not found");
+    else
+      throw "lambdaInvoke not found";
+  }
+
+  if (params.InvocationType == 'RequestResponse') {
+    var t = new Promise((resolve, reject) =>{
+      const context = {
+        succeed: (msg) => resolve(msg),
+        fail: (error) => reject(error)
+      };
+
+      const task = func(JSON.parse(params.Payload), context, (error, response) => {
+        console.log('callback called');
+        if (error) return reject(error);
+        return resolve(response);
+      });
+      if (task instanceof Promise || (task && typeof task.then === 'function')) {
+        return task.then(ret => {
+          if (ret)
+            return resolve(ret);
+          console.log('promise return undefined');
+        })
+        .catch(err => reject(err));
+      }
+    });
+
+    return t.then(ret =>{
+      if( callback )
         callback(null, {
           StatusCode: 200,
-          Payload: JSON.stringify(ret)
+          payload: JSON.stringify(ret)
         });
-      }
-    };
-    var return_error = (err) =>{
-      if( !returned ){
-        returned = true;
-        callback(err);
-      }
-    };
-
-    try{
-      var folder = CONTROLLERS_BASE + params.FunctionName;
-      var func = require(folder).handler;
-
-      const context = {
-        succeed: (msg) => {
-            console.log('succeed called');
-            return_response(msg);
-        },
-        fail: (error) => {
-            console.log('failed called');
-            return_error(error);
-        },
-      };
-
-      const task = func(JSON.parse(params.Payload), context, (error, response) =>{
-        console.log('callback called');
-        if( error )
-          return_error(error);
-        else
-          return_response(response);
-      });
-      if( task instanceof Promise || (task && typeof task.then === 'function') ){
-        return task.then(ret =>{
-          if( ret ){
-              console.log('promise is called');
-              return_response(ret);
-          }else{
-              console.log('promise return undefined');
+      return {
+        promise: async () =>{
+          return {
+            StatusCode: 200,
+            payload: JSON.stringify(ret)
           }
-        })
-        .catch(err =>{
-            console.log('error throwed: ' + err);
-            return_error(err);
-        });
+        }
       }
-    }catch(error){
-      return_error(error);
-    }
-  }else
-  if( params.InvocationType == 'Event' ){
-    callback(null, {
-      StatusCode: 202,
-      Payload: ""
+    })
+    .catch(err =>{
+      if( callback )
+        callback({
+          StatusCode: 200,
+          FunctionError: "Unhandled",
+          payload: JSON.stringify(err)
+      });
+      return {
+        promise: async () =>{
+          return {
+            StatusCode: 200,
+            FunctionError: "Unhandled",
+            payload: JSON.stringify(err)
+          };
+        }
+      };
     });
-    
-    var returned = false;
-    var return_response = (ret) =>{
-      if( !returned ){
-        returned = true;
+  } else
+    if (params.InvocationType == 'Event') {
+      var t = new Promise((resolve, reject) =>{
+        const context = {
+          succeed: (msg) => resolve(msg),
+          fail: (error) => reject(error),
+        };
+
+        const task = func(JSON.parse(params.Payload), context, (error, response) => {
+          console.log('callback called');
+          if (error) return reject(error);
+          resolve(response);
+        });
+        if (task instanceof Promise || (task && typeof task.then === 'function')) {
+          task.then(ret => {
+            if (ret) return resolve(ret);
+            console.log('promise return undefined');
+          })
+          .catch(err => reject(err) );
+        }
+      });
+
+      t.then(ret =>{
         console.log("LambdaProxy OK:", ret);
-      }
-    };
-    var return_error = (err) =>{
-      if( !returned ){
-        returned = true;
+      })
+      .catch(err =>{
         console.log("LambdaProxy Error:", err);
-      }
-    };
-
-    try{
-      var folder = CONTROLLERS_BASE + params.FunctionName;
-      var func = require(folder).handler;
-
-      const context = {
-        succeed: (msg) => {
-            console.log('succeed called');
-            return_response(msg);
-        },
-        fail: (error) => {
-            console.log('failed called');
-            return_error(error);
-        },
-      };
-
-      const task = func(JSON.parse(params.Payload), context, (error, response) =>{
-        console.log('callback called');
-        if( error )
-          return_error(error);
-        else
-          return_response(response);
       });
-      if( task instanceof Promise || (task && typeof task.then === 'function') ){
-        task.then(ret =>{
-          if( ret ){
-              console.log('promise is called');
-              return_response(ret);
-          }else{
-              console.log('promise return undefined');
-          }
-        })
-        .catch(err =>{
-            console.log('error throwed: ' + err);
-            return_error(err);
+
+      if (callback) {
+        callback(null, {
+          StatusCode: 202,
+          Payload: ""
         });
       }
-    }catch(error){
-      return_error(error);
-    }
-  }else
-  if( params.InvocationType == "DryRun" ){
-    callback(null, {
-      StatusCode: 200,
-      Payload: ""
-    });
-  }
+      return {
+        promise: async () => {
+          return {
+            StatusCode: 202,
+            Payload: ""
+          }
+        }
+      };
+    } else
+      if (params.InvocationType == "DryRun") {
+        callback(null, {
+          StatusCode: 200,
+          Payload: ""
+        });
+      }
 }
 
 module.exports = {
