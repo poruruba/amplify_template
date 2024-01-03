3b3f750e07d17a8eccd6e861f22b64018c6cc71a
poruruba <44470781+poruruba@users.noreply.github.com>
2024-01-03 12:30:36 +0900

    Update routing_sqs.js

--- api/controllers/routing/routing_sqs.js
+++ api/controllers/routing/routing_sqs.js
@@ -12,134 +12,139 @@ const DEFAULT_HANDLER = "handler";
 
 const AWS = require('aws-sdk');
 AWS.config.update({
-	region: SQS_REGION
+    region: SQS_REGION
 });
 const sqs = new AWS.SQS({
-	apiVersion: '2012-11-05',
-	endpoint: SQS_ENDPOINT
+    apiVersion: '2012-11-05',
+    endpoint: SQS_ENDPOINT
 });
 
 const fs = require('fs');
 
 function parse_sqs() {
-  // sqs.jsonの検索
-  const folders = fs.readdirSync(CONTROLLERS_BASE);
-  folders.forEach(folder => {
-    try {
-      const stats_dir = fs.statSync(CONTROLLERS_BASE + folder);
-      if (!stats_dir.isDirectory())
-        return;
-    
-      const fname = CONTROLLERS_BASE + folder + "/" + SQS_TARGET_FNAME;
-      if (!fs.existsSync(fname))
-        return;
-      const stats_file = fs.statSync(fname);
-      if (!stats_file.isFile())
-        return;
+    // sqs.jsonの検索
+    const folders = fs.readdirSync(CONTROLLERS_BASE);
+    folders.forEach(folder => {
+        try {
+            const stats_dir = fs.statSync(CONTROLLERS_BASE + folder);
+            if (!stats_dir.isDirectory())
+                return;
 
-      // pollerの登録
-      const defs = JSON.parse(fs.readFileSync(fname).toString());
-      parse_sqs_json(defs, CONTROLLERS_BASE + folder, folder);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+            const fname = CONTROLLERS_BASE + folder + "/" + SQS_TARGET_FNAME;
+            if (!fs.existsSync(fname))
+                return;
+            const stats_file = fs.statSync(fname);
+            if (!stats_file.isFile())
+                return;
+
+            // pollerの登録
+            const defs = JSON.parse(fs.readFileSync(fname).toString());
+            parse_sqs_json(defs, CONTROLLERS_BASE + folder, folder);
+        } catch (error) {
+            console.log(error);
+        }
+    });
 }
 
 function parse_sqs_json(defs, folder, folder_name) {
-  defs.forEach(item => {
-    if (!item.enable )
-      return;
-      
-    const handler = item.handler || DEFAULT_HANDLER;
-    const proc = require(folder)[handler];
-    const params = {
-      QueueUrl: item.QueueUrl,
-      AttributeNames: item.AttributeNames,
-      MaxNumberOfMessages: (item.MaxNumberOfMessages === undefined) ? 1 : item.MaxNumberOfMessages,
-      MessageAttributeNames: item.MessageAttributeNames,
-      VisibilityTimeout: (item.VisibilityTimeout === undefined) ? 30 : item.VisibilityTimeout,
-      WaitTimeSeconds: (item.WaitTimeSeconds === undefined) ? 20: item.WaitTimeSeconds
-    };
+    defs.forEach(item => {
+        if (!item.enable)
+            return;
 
-    continuousReceive(params, proc);
+        const handler = item.handler || DEFAULT_HANDLER;
+        const proc = require(folder)[handler];
+        const params = {
+            QueueUrl: item.QueueUrl,
+            AttributeNames: item.AttributeNames,
+            MaxNumberOfMessages: (item.MaxNumberOfMessages === undefined) ? 1 : item.MaxNumberOfMessages,
+            MessageAttributeNames: item.MessageAttributeNames,
+            VisibilityTimeout: (item.VisibilityTimeout === undefined) ? 30 : item.VisibilityTimeout,
+            WaitTimeSeconds: (item.WaitTimeSeconds === undefined) ? 20 : item.WaitTimeSeconds
+        };
 
-    console.log("sqs(" + item.QueueUrl + ") " + handler + ' ' + folder_name);
-  });
+        continuousReceive(params, proc);
+
+        console.log("sqs(" + item.QueueUrl + ") " + handler + ' ' + folder_name);
+    });
 }
 
-async function continuousReceive(params, func){
-	while(true){
-		try{
-			await new Promise((resolve, reject) =>{
-				sqs.receiveMessage(params, async (err, data) => {
-				  if (err) {
-				    return reject(err);
-				  }
-				  if( !data.Messages ){
-				  	return resolve('message empty');
-				  }
+async function continuousReceive(params, func) {
+    while (true) {
+        try {
+            await new Promise((resolve, reject) => {
+                sqs.receiveMessage(params, async (err, data) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    if (!data.Messages) {
+                        return resolve('message empty');
+                    }
 
-          try{
-			  		const event = {
-			  			Records: data.Messages.map(item => {
-                return {
-                  messageId: item.MessageId,
-                  receiptHandle: item.ReceiptHandle,
-                  body :item.Body,
-                  attributes: item.Attributes,
-                  messageAttributes: item.MessageAttributes,
-                  md5OfBody: item.MD5OfBody,
-                  md5OfMessageAttributes: item.MD5OfMessageAttributes,
-                  awsRegion: SQS_REGION
-                }
-              })
-			  		};
+                    try {
+                        const event = {
+                            Records: data.Messages.map(item => {
+                                return {
+                                    messageId: item.MessageId,
+                                    receiptHandle: item.ReceiptHandle,
+                                    body: item.Body,
+                                    attributes: item.Attributes,
+                                    messageAttributes: item.MessageAttributes,
+                                    md5OfBody: item.MD5OfBody,
+                                    md5OfMessageAttributes: item.MD5OfMessageAttributes,
+                                    awsRegion: SQS_REGION
+                                }
+                            })
+                        };
 
-            const success_return = (msg) => {
-              if( data.Messages ){
-                const deleteParams = {
-                  QueueUrl: params.QueueUrl,
-                  Entries: data.Messages.map(item => { return { Id: item.MessageId, ReceiptHandle: item.ReceiptHandle } } )
-                };
-                sqs.deleteMessageBatch(deleteParams, (err, data) => {
-                  if (err) {
-                    return reject(err);
-                  } else {
-                    return resolve();
-                  }
+                        const success_return = (msg) => {
+                            if (data.Messages) {
+                                const deleteParams = {
+                                    QueueUrl: params.QueueUrl,
+                                    Entries: data.Messages.map(item => {
+                                        return {
+                                            Id: item.MessageId,
+                                            ReceiptHandle: item.ReceiptHandle
+                                        }
+                                    })
+                                };
+                                sqs.deleteMessageBatch(deleteParams, (err, data) => {
+                                    if (err) {
+                                        return reject(err);
+                                    } else {
+                                        return resolve();
+                                    }
+                                });
+                            }
+                        };
+                        const context = {
+                            succeed: success_return,
+                            fail: (error) => resolve(error)
+                        };
+                        const task = func(event, context, (error, response) => {
+                            if (error) return resolve(error);
+                            return success_return(response);
+                        });
+                        if (task instanceof Promise || (task && typeof task.then === 'function')) {
+                            return task.then(ret => {
+                                    if (ret)
+                                        return success_return(ret);
+                                })
+                                .catch(err => resolve(err));
+                        } else {
+                            if (task !== undefined)
+                                success_return(task);
+                        }
+                    } catch (error) {
+                        console.log(error);
+                        return reject(err);
+                    }
                 });
-              }
-            };
-            const context = {
-              succeed: success_return,
-              fail: (error) => resolve(error)
-            };
-            const task = func(event, context, (error, response) => {
-              if (error) return resolve(error);
-              return success_return(response);
             });
-            if (task instanceof Promise || (task && typeof task.then === 'function')) {
-              return task.then(ret => {
-                if (ret)
-                  return success_return(ret);
-              })
-              .catch(err => resolve(err));
-            }else{
-              if( task !== undefined )
-                success_return(task);
-            }
-          }catch(error){
+        } catch (error) {
             console.log(error);
-            return reject(err);
-          }
-				});
-			});
-		}catch(error){
-			console.log(error);
-			return;
-		}
-	}
+            return;
+        }
+    }
 }
 
 module.exports = parse_sqs();
