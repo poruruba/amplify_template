d3310a3569762e0b473f435769a662c5d811c7f2
poruruba <44470781+poruruba@users.noreply.github.com>
2025-05-25 21:43:13 +0900

    Delete template/api/test-get-mjs/index.js

--- template/api/test-get-mjs/index.js
+++
@@ -1,8 +0,0 @@
-const HELPER_BASE = "../../helpers/opt/" || "/opt/";
-import Response from '../../helpers/opt/response.mjs';
-import Redirect from '../../helpers/opt/redirect.mjs';
-
-export const handler = async (event, context, callback) => {
-	console.log(event.queryStringParameters);
-	return new Response({ message: 'Hello World' });
-};
