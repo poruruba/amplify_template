dd7e6dd5f746898e81a7e79b783f469232686d52
poruruba <44470781+poruruba@users.noreply.github.com>
2025-12-06 14:38:16 +0900

    Update vue_utils.js

--- template/html/template/js/vue_utils.js
+++ template/html/template/js/vue_utils.js
@@ -86,7 +86,7 @@ async function do_http(input){
     body = new URLSearchParams(input.params);
   }else if( content_type == "multipart/form-data"){
     body = Object.entries(input.params).reduce((l, [k, v]) => { if(v !== undefined) l.append(k, v); return l; }, new FormData());
-  }else if( content_type == "application/octet-stream" ){
+  }else{
     body = input.body;
   }
 
